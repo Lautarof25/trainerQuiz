f1a5ba44362aef6b4aea56149a9649027616e5c6
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-08-14 13:02:35 -0300

    adding frontend flow

--- src/js/dom.js
+++ src/js/dom.js
@@ -2,14 +2,23 @@
 const home = document.querySelector('#home')
 const createNumberQuizzes = document.querySelector('#createNumberQuizzes')
 const createQuizzes = document.querySelector('#createQuizzes')
+const selectQuiz = document.querySelector('#selectQuiz')
+const playQuizz = document.querySelector('#playQuizz')
+const result = document.querySelector('#result')
+const spanResult = document.querySelector('#spanResult')
+const popUp = document.querySelector('#popUp')
 // Buttons
 const buttonCreate = document.querySelector('#buttonCreate')
 const buttonPlay = document.querySelector('#buttonPlay')
 const buttonNext = document.querySelector('#buttonNext')
 const buttonBackHome = document.querySelector('#buttonBackHome')
 const buttonBackCreate = document.querySelector('#buttonBackCreate')
+const buttonFinish = document.querySelector('#buttonFinish')
+const buttonStart = document.querySelector('#buttonStart')
+const backHomeFinish = document.querySelector('#backHomeFinish')
+// Options quizzes
+const select = document.querySelector('#select')
+const option1 = document.querySelector('#option1')
+const option2 = document.querySelector('#option2')
 // input number questions
 const inputNumber = document.querySelector('#inputNumber')
-
-
-
--- src/js/events.js
+++ src/js/events.js
@@ -34,3 +34,61 @@ function backCreate() {
     createNumberQuizzes.classList.toggle('d-none')
     createQuizzes.classList.toggle('d-none')
 }
+
+buttonFinish.addEventListener('click',saveAndBackHome)
+
+function saveAndBackHome(){
+    createQuizzes.classList.toggle('d-none')
+    home.classList.toggle('d-none')
+}
+
+buttonPlay.addEventListener('click',startQuizzes)
+
+function startQuizzes(){
+    home.classList.toggle('d-none')
+    selectQuiz.classList.toggle('d-none')
+}
+
+select.addEventListener('change', optionSelectedOk)
+
+function optionSelectedOk(){
+    if(select.value === "holidays"){
+        return true
+    }
+}
+
+buttonStart.addEventListener('click',playQuizzes)
+
+function playQuizzes() {
+    if(optionSelectedOk()){
+        selectQuiz.classList.toggle('d-none')
+        playQuiz.classList.toggle('d-none')
+    }else {
+        popUp.classList.toggle('d-none')
+        setTimeout(() => {
+            popUp.classList.toggle('d-none')
+        }, 2500);
+    }
+}
+
+option1.addEventListener('click',hitAnswer)
+option2.addEventListener('click',wrongAnswer)
+
+function hitAnswer(){
+    spanResult.textContent = "100"
+    playQuiz.classList.toggle('d-none')
+    result.classList.toggle('d-none')
+}
+
+function wrongAnswer(){
+    spanResult.textContent = "0"
+    playQuiz.classList.toggle('d-none')
+    result.classList.toggle('d-none')
+}
+
+backHomeFinish.addEventListener("click", backHomeFinishPage)
+
+function backHomeFinishPage(){
+    home.classList.toggle("d-none")
+    result.classList.toggle('d-none')
+}
